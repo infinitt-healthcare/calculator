e251d8bdc3040a0e5343fe95b6c08860471e8b7c
masungil70 <masungil70@gmail.com>
2022-04-19 17:50:10 +0900

    더하기 기능 구현

--- calculatorDlg.cpp
+++ calculatorDlg.cpp
@@ -60,13 +60,28 @@ CcalculatorDlg::CcalculatorDlg(CWnd* pParent /*=nullptr*/)
 void CcalculatorDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
-	DDX_Text(pDX, IDC_EDIT1, m_value,100);
+	DDX_Text(pDX, IDC_EDIT1, m_value);
 }
 
 BEGIN_MESSAGE_MAP(CcalculatorDlg, CDialog)
 	ON_WM_SYSCOMMAND()
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTON1, &CcalculatorDlg::OnBnClickedButton1)
+	ON_BN_CLICKED(IDC_BUTTON2, &CcalculatorDlg::OnBnClickedButton2)
+	ON_BN_CLICKED(IDC_BUTTON3, &CcalculatorDlg::OnBnClickedButton3)
+	ON_BN_CLICKED(IDC_BUTTON4, &CcalculatorDlg::OnBnClickedButton4)
+	ON_BN_CLICKED(IDC_BUTTON5, &CcalculatorDlg::OnBnClickedButton5)
+	ON_BN_CLICKED(IDC_BUTTON6, &CcalculatorDlg::OnBnClickedButton6)
+	ON_BN_CLICKED(IDC_BUTTON7, &CcalculatorDlg::OnBnClickedButton7)
+	ON_BN_CLICKED(IDC_BUTTON8, &CcalculatorDlg::OnBnClickedButton8)
+	ON_BN_CLICKED(IDC_BUTTON9, &CcalculatorDlg::OnBnClickedButton9)
+	ON_BN_CLICKED(IDC_BUTTON10, &CcalculatorDlg::OnBnClickedButton10)
+	ON_BN_CLICKED(IDC_BUTTON11, &CcalculatorDlg::OnBnClickedButton11)
+	ON_BN_CLICKED(IDC_BUTTON12, &CcalculatorDlg::OnBnClickedButton12)
+	ON_BN_CLICKED(IDC_BUTTON13, &CcalculatorDlg::OnBnClickedButton13)
+	ON_BN_CLICKED(IDC_BUTTON14, &CcalculatorDlg::OnBnClickedButton14)
+	ON_BN_CLICKED(IDC_BUTTON15, &CcalculatorDlg::OnBnClickedButton15)
 	ON_BN_CLICKED(IDC_BUTTON16, &CcalculatorDlg::OnBnClickedButton16)
 END_MESSAGE_MAP()
 
@@ -157,19 +172,111 @@ HCURSOR CcalculatorDlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
+void CcalculatorDlg::AddNumber(LPCTSTR lpszNumber)
+{
+	UpdateData();
+	if (m_value == _T("값을 입력해주세요.")) {
+		m_value = _T("");
+	}
+	m_value += lpszNumber;
+	UpdateData(FALSE);
+}
 
+void CcalculatorDlg::OnBnClickedButton1()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	AddNumber(_T("1"));
+}
+
+void CcalculatorDlg::OnBnClickedButton2()
+{
+	AddNumber(_T("2"));
+}
+
+void CcalculatorDlg::OnBnClickedButton3()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+}
+
+void CcalculatorDlg::OnBnClickedButton4()
+{
+	AddNumber(_T("4"));
+}
+
+void CcalculatorDlg::OnBnClickedButton5()
+{
+	AddNumber(_T("5"));
+}
+
+void CcalculatorDlg::OnBnClickedButton6()
+{
+	UpdateData();
+	m_op1 = m_value;
+	m_op = 1;
+	m_value = _T("");
+	UpdateData(FALSE);
+}
+
+void CcalculatorDlg::OnBnClickedButton7()
+{
+	AddNumber(_T("7"));
+}
+
+void CcalculatorDlg::OnBnClickedButton8()
+{
+	AddNumber(_T("8"));
+}
+
+void CcalculatorDlg::OnBnClickedButton9()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+}
+
+void CcalculatorDlg::OnBnClickedButton10()
+{
+	AddNumber(_T("3"));
+}
+
+void CcalculatorDlg::OnBnClickedButton11()
+{
+	AddNumber(_T("6"));
+}
+
+void CcalculatorDlg::OnBnClickedButton12()
+{
+	AddNumber(_T("9"));
+}
+
+void CcalculatorDlg::OnBnClickedButton13()
+{
+	UpdateData();
+	m_op1 = m_value;
+}
+
+void CcalculatorDlg::OnBnClickedButton14()
+{
+	AddNumber(_T("0"));
+}
+
+void CcalculatorDlg::OnBnClickedButton15()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+}
 
 void CcalculatorDlg::OnBnClickedButton16()
 {
-	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
 	UpdateData();
+	m_op2 = m_value;
+	int result = 0;
 
-	if (m_value){
-		MessageBox(_T("값 = %d", result));
-		CDialog::OnOK();
+	switch (m_op) {
+	case 1:
+		result = _ttoi(m_op1.GetBuffer()) + _ttoi(m_op2.GetBuffer());
+		break;
 	}
-	else {
-		MessageBox(_T("값을 입력해주세요."));
 
-	}
+
+	m_value.Format(_T("%d"), result);
+	UpdateData(FALSE);
+
 }
--- calculatorDlg.h
+++ calculatorDlg.h
@@ -4,7 +4,8 @@
 
 #pragma once
 
-
+//int add(int a, int b);
+//20 = op1 + op2;
 // CcalculatorDlg 대화 상자
 class CcalculatorDlg : public CDialog
 {
@@ -20,6 +21,7 @@ public:
 	protected:
 	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
 
+	void AddNumber(LPCTSTR lpszNumber);
 
 // 구현입니다.
 protected:
@@ -33,5 +35,24 @@ protected:
 	DECLARE_MESSAGE_MAP()
 public:
 	CString m_value;
+	int		m_op;
+	CString m_op1;
+	CString m_op2;
+
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedButton2();
+	afx_msg void OnBnClickedButton3();
+	afx_msg void OnBnClickedButton4();
+	afx_msg void OnBnClickedButton5();
+	afx_msg void OnBnClickedButton6();
+	afx_msg void OnBnClickedButton7();
+	afx_msg void OnBnClickedButton8();
+	afx_msg void OnBnClickedButton9();
+	afx_msg void OnBnClickedButton10();
+	afx_msg void OnBnClickedButton11();
+	afx_msg void OnBnClickedButton12();
+	afx_msg void OnBnClickedButton13();
+	afx_msg void OnBnClickedButton14();
+	afx_msg void OnBnClickedButton15();
 	afx_msg void OnBnClickedButton16();
 };
